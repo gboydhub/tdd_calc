1711b963ecdc50fafd10d2d9ec9c69ada3f37b14
Gary Boyd <eponick@gmail.com>
2018-06-20 14:48:06 -0400

    Test passing multiple types, pass

--- calc.rb
+++ calc.rb
@@ -13,18 +13,22 @@ def calc_check_if_numbers?(a, b)
 end
 
 def calc_add(a, b)
+    unless calc_check_if_numbers?(a, b); return false; end
     a+b
 end
 
 def calc_subtract(a, b)
+    unless calc_check_if_numbers?(a, b); return false; end
     a-b
 end
 
 def calc_multiply(a, b)
+    unless calc_check_if_numbers?(a, b); return false; end
     a*b
 end
 
 def calc_divide(a, b)
+    unless calc_check_if_numbers?(a, b); return false; end
     if b==0; return false; end
     a/b
 end
