f3cfaa61bf2726c4b3f26d1a7cac08dc6bd35dae
Gary Boyd <eponick@gmail.com>
2018-06-20 13:49:18 -0400

    Test 1+1=2, fail

--- test_calc.rb
+++ test_calc.rb
@@ -5,4 +5,8 @@ class TestTddCalc < Minitest::Test
     def test_assert_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_one_plus_one_is_two
+        assert_equal(2, calc_add(1, 1))
+    end
 end
