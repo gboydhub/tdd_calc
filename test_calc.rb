1a524b9a9911b945e51c8cecd92b14065b002eaa
Gary Boyd <eponick@gmail.com>
2018-06-20 13:52:04 -0400

    Test add 3+3=6, fail

--- test_calc.rb
+++ test_calc.rb
@@ -9,4 +9,8 @@ class TestTddCalc < Minitest::Test
     def test_one_plus_one_is_two
         assert_equal(2, calc_add(1, 1))
     end
+
+    def test_3_plus_3_returns_six
+        assert_equal(6, calc_add(3, 3))
+    end
 end
