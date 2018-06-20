c46e08aab61caf0ccef8f69aa15df913d2ccec18
Gary Boyd <eponick@gmail.com>
2018-06-20 13:45:56 -0400

    Test working

--- test_calc.rb
+++ test_calc.rb
@@ -1,5 +1,8 @@
 require "minitest/autorun"
-require_relative "tdd_fizz.rb"
+require_relative "calc.rb"
 
 class TestTddCalc < Minitest::Test
+    def test_assert_1_equals_1
+        assert_equal(1, 1)
+    end
 end
